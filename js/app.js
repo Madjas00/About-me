3fcec88329d495193ac9e19a5104723c68780f12
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-05 16:35:41 -0500

    questions placed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
-
+//question 1
 var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
-var sanitizedAnswer = a1 .toUpperCase().trim();
+var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
@@ -14,9 +14,54 @@ else if (sanitizedAnswer === 'N'
 else{
     alert('That is not a valid response! You imbecile!')
 }
-
+//question 2
 var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
-var sanitizedAnswer = a1 .toUpperCase().trim();
+var sanitizedAnswer = a1 .toUpperCase() .trim();
+
+if (sanitizedAnswer === 'Y'
+|| sanitizedAnswer === 'YES') {
+    alert('You answered:' +a1+ '  I am sorry to inform you but that is not true');
+}
+else if (sanitizedAnswer === 'N'
+|| sanitizedAnswer === 'NO') {
+    alert(a1 + ' No Sir.')
+}
+else{
+    alert('That is not a valid response! You imbecile!')
+}
+//question 3
+var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
+var sanitizedAnswer = a1 .toUpperCase() .trim();
+
+if (sanitizedAnswer === 'Y'
+|| sanitizedAnswer === 'YES') {
+    alert('You answered:' +a1+ '  I am sorry to inform you but that is not true');
+}
+else if (sanitizedAnswer === 'N'
+|| sanitizedAnswer === 'NO') {
+    alert(a1 + ' No Sir.')
+}
+else{
+    alert('That is not a valid response! You imbecile!')
+}
+//question 4
+var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
+var sanitizedAnswer = a1 .toUpperCase() .trim();
+
+if (sanitizedAnswer === 'Y'
+|| sanitizedAnswer === 'YES') {
+    alert('You answered:' +a1+ '  I am sorry to inform you but that is not true');
+}
+else if (sanitizedAnswer === 'N'
+|| sanitizedAnswer === 'NO') {
+    alert(a1 + ' No Sir.')
+}
+else{
+    alert('That is not a valid response! You imbecile!')
+}
+//question5
+var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
+var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
