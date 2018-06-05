d4ade265857ea86a7c791a276d2bf0f3886cfe6d
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-05 16:25:10 -0500

    Getting questions worked out

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var qOne = prompt('yes or no');
-var sanitizedAnswer = qOne .toUpperCase().trim();
+var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
+var sanitizedAnswer = a1 .toUpperCase().trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
-    alert(qOne+ '. Yes Sir');
+    alert('You answered:' +a1+ '  I am sorry to inform you but that is not true');
 }
+else if (sanitizedAnswer === 'N'
+|| sanitizedAnswer === 'NO') {
+    alert(a1 + ' No Sir.')
+}
+else{
+    alert('That is not a valid response! You imbecile!')
+}
+
+var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
+var sanitizedAnswer = a1 .toUpperCase().trim();
+
+if (sanitizedAnswer === 'Y'
+|| sanitizedAnswer === 'YES') {
+    alert('You answered:' +a1+ '  I am sorry to inform you but that is not true');
+}
+else if (sanitizedAnswer === 'N'
+|| sanitizedAnswer === 'NO') {
+    alert(a1 + ' No Sir.')
+}
+else{
+    alert('That is not a valid response! You imbecile!')
+}
+
