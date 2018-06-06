56ed063d0941eb18461ff48acc54e6e211fa43bb
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-05 22:20:24 -0500

    WIP

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 'use strict';
+//greeting
+var name = prompt('Welcome to my About-Me quiz. What is your name?');
+    console.log('name? ' + name);
 //question 1
-var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
+var a1 = prompt('Okay ' +name+ '. First question. What is the alias that I most often go by?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
@@ -12,8 +15,9 @@ else if (sanitizedAnswer === 'N'
     alert(a1 + ' No Sir.')
 }
 else{
-    alert('That is not a valid response! You imbecile!')
+    alert('I am not familiar with that answer.')
 }
+console.log ('answer1 ' +a1)
 //question 2
 var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
@@ -59,7 +63,7 @@ else if (sanitizedAnswer === 'N'
 else{
     alert('That is not a valid response! You imbecile!')
 }
-//question5
+//question 5
 var a1 = prompt('Am I the world record holder for most spaghetti eaten in one sitting?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
