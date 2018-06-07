9bb29467a75012bfe740c5b041bc07bc4bf96ba2
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-06 20:35:57 -0500

    score

--- js/app.js
+++ js/app.js
@@ -1,85 +1,92 @@
 'use strict';
 //greeting
 var name = prompt('Welcome to my About-Me quiz. What is your name?');
-    console.log('name? ' + name);
+var score = 0;
+console.log('name? ' + name);
 //question 1
 var a1 = prompt('Okay, ' +name+ '. Am I over the age of 25?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
-    alert('You answered ' +a1+ '. That is correct ' +name+ '! I am actually 28.');
+  alert('You answered ' +a1+ '. That is correct ' +name+ '! I am actually 28.'); score++;
 }
 else if (sanitizedAnswer === 'N'
 || sanitizedAnswer === 'NO') {
-    alert('You answered ' +a1+ '. Sorry ' +name+ ' but that is incorrect.')
+  alert('You answered ' +a1+ '. Sorry ' +name+ ' but that is incorrect.');
 }
 else{
-    alert('I am not familiar with that answer.')
+  alert('I am not familiar with that answer.');
 }
 console.log ('answer1 ' +a1);
+console.log ('score = ' +score);
 //question 2
 var a1 = prompt('Next question, ' +name+ '. Do I have a tattoo on my forearm?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
-    alert('You answered ' +a1+ '. That is correct! I do have a tattoo on my right forearm.');
+  alert('You answered ' +a1+ '. That is correct! I do have a tattoo on my right forearm.'); score++;
 }
 else if (sanitizedAnswer === 'N'
 || sanitizedAnswer === 'NO') {
-    alert('That is incorrect. I do have a tattoo on my right forearm.')
+  alert('That is incorrect. I do have a tattoo on my right forearm.');
 }
 else{
-    alert('That is not a valid response! You imbecile!')
+  alert('That is not a valid response! You imbecile!');
 }
 console.log ('answer2 ' +a1);
+console.log ('score = ' +score);
 //question 3
 var a1 = prompt('Alright, ' +name+ '. Next question. Do I speak a foreign language?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
-    alert('I wish the answer was correct, but I do not know any foreign languages.');
+  alert('I wish the answer was correct, but I do not know any foreign languages.');
 }
 else if (sanitizedAnswer === 'N'
 || sanitizedAnswer === 'NO') {
-    alert('You are right. I do not speak any foreign languages.')
+  alert('You are right. I do not speak any foreign languages.'); score++;
 }
 else{
-    alert('That is not a valid response! You imbecile!')
+  alert('That is not a valid response! You imbecile!');
 }
 console.log ('answer3 ' +a1);
+console.log ('score= ' +score);
 //question 4
 var a1 = prompt('Alright, ' +name+ '. Do I know the muffin man?');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
-    alert('You answered ' +a1+ '. That is correct! He lives on Drury Lane!');
+  alert('You answered ' +a1+ '. That is correct! He lives on Drury Lane!'); score++;
 }
 else if (sanitizedAnswer === 'N'
 || sanitizedAnswer === 'NO') {
-    alert('While I do not know him personally, I am aware of him')
+  alert('While I do not know him personally, I am aware of him');
 }
 else{
-    alert('That is not a valid response! You imbecile!')
+  alert('That is not a valid response! You imbecile!');
 }
-console.log ('answer4 ' +a1);
+console.log ('answer4' +a1);
+console.log ('score= ' +score);
 //question 5
 var a1 = prompt('Am I the world record holder for "Most Spaghetti Eaten In One Sitting?"');
 var sanitizedAnswer = a1 .toUpperCase() .trim();
 
 if (sanitizedAnswer === 'Y'
 || sanitizedAnswer === 'YES') {
-    alert('Unfortunately I am not');
+  alert('Unfortunately I am not');
 }
 else if (sanitizedAnswer === 'N'
 || sanitizedAnswer === 'NO') {
-    alert('It pains me to admit it, but you are correct.')
+  alert('It pains me to admit it, but you are correct.'); score++;
 }
 else{
-    alert('That is not a valid response! You imbecile!')
+  alert('That is not a valid response! You imbecile!');
 }
 console.log ('answer5 ' +a1);
+console.log ('score= ' +score);
+
 
