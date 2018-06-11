9e6426f8fc94550d203fac69ae9debc46b7dc960
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-10 22:27:47 -0500

    second list

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ function askQuestion1(questionText, correctWord, correctMessage, incorrectMessag
     alert(correctMessage);
 
   } else {
-    alert(incorrectMessage + nameU);
+    alert(incorrectMessage);
   }
 
 }
@@ -30,91 +30,11 @@ askQuestion1(questionText, correctWord, correctMessage, incorrectMessage);
 
 askQuestion1('Do I have a tattoo?' , 'yes' , 'That is correct! I have a tattoo on my right forearm' , 'That is incorrect. I have a tattoo on my right forearm');
 
-//question 1
-// var a1 = prompt('Okay, ' +name+ '. Am I over the age of 25?');
-// var sanitizedAnswer = a1 .toUpperCase() .trim();
+askQuestion1('Do I speak a foreign language?' , 'no' , 'That is correct! I would love to learn one though' , 'Incorrect. While I wish I did speak a foreign language I do not.');
 
-// if (sanitizedAnswer === 'Y'
-// || sanitizedAnswer === 'YES') {
-//   alert('You answered ' +a1+ '. That is correct ' +name+ '! I am actually 28.'); score++;
-// }
-// else if (sanitizedAnswer === 'N'
-// || sanitizedAnswer === 'NO') {
-//   alert('You answered ' +a1+ '. Sorry ' +name+ ' but that is incorrect.');
-// }
-// else{
-//   alert('I am not familiar with that answer.');
-// }
-// console.log ('answer1 ' +a1);
-// console.log ('score = ' +score);
-//question 2
-// var a1 = prompt('Next question, ' +name+ '. Do I have a tattoo on my forearm?');
-// var sanitizedAnswer = a1 .toUpperCase() .trim();
+askQuestion1('Alright, ' +nameU+ '. Do I know the muffin man?', 'yes' , 'That is correct. He lives on Drury lane.' , 'While I do not know him personally I am aware of him.' );
 
-// if (sanitizedAnswer === 'Y'
-// || sanitizedAnswer === 'YES') {
-//   alert('You answered ' +a1+ '. That is correct! I do have a tattoo on my right forearm.'); score++;
-// }
-// else if (sanitizedAnswer === 'N'
-// || sanitizedAnswer === 'NO') {
-//   alert('That is incorrect. I do have a tattoo on my right forearm.');
-// }
-// else{
-//   alert('That is not a valid response! You imbecile!');
-// }
-// console.log ('answer2 ' +a1);
-// console.log ('score = ' +score);
-//question 3
-// var a1 = prompt('Alright, ' +name+ '. Next question. Do I speak a foreign language?');
-// var sanitizedAnswer = a1 .toUpperCase() .trim();
-
-// if (sanitizedAnswer === 'Y'
-// || sanitizedAnswer === 'YES') {
-//   alert('I wish the answer was correct, but I do not know any foreign languages.');
-// }
-// else if (sanitizedAnswer === 'N'
-// || sanitizedAnswer === 'NO') {
-//   alert('You are right. I do not speak any foreign languages.'); score++;
-// }
-// else{
-//   alert('That is not a valid response! You imbecile!');
-// }
-// console.log ('answer3 ' +a1);
-// console.log ('score= ' +score);
-//question 4
-// var a1 = prompt('Alright, ' +name+ '. Do I know the muffin man?');
-// var sanitizedAnswer = a1 .toUpperCase() .trim();
-
-// if (sanitizedAnswer === 'Y'
-// || sanitizedAnswer === 'YES') {
-//   alert('You answered ' +a1+ '. That is correct! He lives on Drury Lane!'); score++;
-// }
-// else if (sanitizedAnswer === 'N'
-// || sanitizedAnswer === 'NO') {
-//   alert('While I do not know him personally, I am aware of him');
-// }
-// else{
-//   alert('That is not a valid response! You imbecile!');
-// }
-// console.log ('answer4' +a1);
-// console.log ('score= ' +score);
-//question 5
-// var a1 = prompt('Am I the world record holder for "Most Spaghetti Eaten In One Sitting?"');
-// var sanitizedAnswer = a1 .toUpperCase() .trim();
-
-// if (sanitizedAnswer === 'Y'
-// || sanitizedAnswer === 'YES') {
-//   alert('Unfortunately I am not');
-// }
-// else if (sanitizedAnswer === 'N'
-// || sanitizedAnswer === 'NO') {
-//   alert('It pains me to admit it, but you are correct.'); score++;
-// }
-// else{
-//   alert('That is not a valid response! You imbecile!');
-// }
-// console.log ('answer5 ' +a1);
-// console.log ('score= ' +score);
+askQuestion1('Am i the world record holder for "most spaghetti eaten in one sitting?"' , 'no' , 'I wish I was but sadly I am not.' , 'That is nice of you to think so. Unfortunately I am not.');
 
 //question 6
 
